d8d588b69ba08b780b13144a32db4a6636644792
fernandeslouro <jdlouro@gmv.com>
2021-03-11 12:45:13 +0000

    start implementing

---
+++ nn.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+INPUT_SIZE = 16
+OUTPUR_SIZE = 10
+
+
+class fully_connected_layer(self, layer_size):
+    def __init__(self):
+        self.weights = 
+
+
+input_layer = np.array(IMPUT_SIZE)
+
+
+output_layer = np.array(OUTPUT_SIZE)
+
