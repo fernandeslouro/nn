49b00c1624d7e5998eac5d3797ba8d4a0ad16a41
João Diogo Fernandes Louro <jdlouro@gmv.com>
2021-09-02 20:47:04 +0100

    fix to respect PEP8

--- nn.py
+++ nn.py
@@ -13,13 +13,23 @@ NUMBER_HIDDEN_LAYERS = 7
 HIDDEN_LAYER_SIZE = 100
 BATCH_SIZE = 10
 
-trans = torchvision.transforms.Compose([torchvision.transforms.ToTensor(), 
-                torchvision.transforms.Normalize((0.5,), (1.0,))])
+trans = torchvision.transforms.Compose([
+            torchvision.transforms.ToTensor(),
+            torchvision.transforms.Normalize((0.5,), (1.0,))])
 
-train_set = torchvision.datasets.MNIST(root=".", train=True, transform=trans, download=True)
-test_set = torchvision.datasets.MNIST(root=".", train=False, transform=trans, download=True)
+train_set = torchvision.datasets.MNIST(
+        root=".",
+        train=True,
+        transform=trans,
+        download=True)
+
+test_set = torchvision.datasets.MNIST(
+        root=".",
+        train=False,
+        transform=trans,
+        download=True)
 # %%
-# these loades will output a list of 2 elements, each of them a 
+# these loades will output a list of 2 elements, each of them a
 # torch tenson (batch size x picture, batch size x number)
 train_loader = torch.utils.data.DataLoader(
                  dataset=train_set,
@@ -33,20 +43,30 @@ test_loader = torch.utils.data.DataLoader(
 example = iter(train_loader).next()
 
 # %%
-# The backpropagation algorithm works by computing the gradient of the loss function with respect to each weight by the chain rule, computing the gradient one layer at a time, iterating backward from the last layer to avoid redundant calculations of intermediate terms in the chain rule
-image = example[0][0][0].numpy() # it's a 6
+# The backpropagation algorithm works by computing the gradient of the loss
+# function with respect to each weight by the chain rule, computing the
+# gradient one layer at a time, iterating backward from the last layer
+# to avoid redundant calculations of intermediate terms in the chain rule
+
+image = example[0][0][0].numpy()  # it's a 6
 label = np.array([0, 0, 0, 0, 0, 0, 1, 0, 0, 0])
 criterion = cross_entropy_loss
-#optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-nn = neural_network(IMAGE_SIZE**2, OUTPUT_SIZE, NUMBER_HIDDEN_LAYERS, HIDDEN_LAYER_SIZE)
+# optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+nn = neural_network(
+        IMAGE_SIZE**2,
+        OUTPUT_SIZE,
+        NUMBER_HIDDEN_LAYERS,
+        HIDDEN_LAYER_SIZE)
+
 outputs, activations = nn.predict(image)
 loss = criterion(outputs, label)
 # %%
-# Let G be the gradient on the unnormalized log probabilities 
+# Let G be the gradient on the unnormalized log probabilities
 # U(2) provided by the cross_entropy operation
 g = loss
-#gradients[nn.number_hidden_layers] = g
+# gradients[nn.number_hidden_layers] = g
 wb_grad = [g]
 i = 1
 
@@ -55,26 +75,27 @@ i = 1
 for k in reversed(nn.hidden):
     print(i)
     # Convert the gradient on the layer’s output into a gradient into the pre-
-    # nonlinearity activation (element-wise multiplication if f is element-wise)
+    # nonlinearity activation, element-wise multiplication if f is element-wise
 
     g = np.multiply(g, relu_derivative(activations[i-1]))
     print(g)
-    # Compute gradients on weights and biases 
+    # Compute gradients on weights and biases
     act_prev_l = activations[i-1]
     der_nonlin_z = relu_derivative(np.maximum(0, activations[i-1]))
     # WEIGHTS GRADIENT AT EACH LAYER
-    # activation of previous layer * 
+    # activation of previous layer *
     # derivative of non-lineariry of z (w*a(l-1)+ b) *
-    # derivative of the cost funtion of current activation - gradient of next layer
+    # derivative of the cost funtion
+    # of current activation - gradient of next layer
     weight_gradients = g * np.transpose(activations[i-2])
     print(weight_gradients)
 
     # BIAS GRADIENT AT EACH LAYER
-    # 1 * 
+    # 1 *
     # derivative of non-lineariry of z (w*a(l-1)+ b) *
-    # derivative of the cost funtion of current activation - gradient of next layer
+    # derivative of the cost funtion of current
+    # activation - gradient of next layer
     bias_gradients = g
-
 
     wb_grad.insert(0, np.array([weight_gradients, bias_gradients]))
     i += 1
@@ -82,11 +103,9 @@ for k in reversed(nn.hidden):
 
 # make gradient update
 for i, layer in enumerate(nn.hidden):
-    if i>0:
+    if i > 0:
         layer.weights += wb_grad[i-1][0]
         layer.bias += wb_grad[i-1][1]
-
-
 
 '''
 for epoch in range(50):  # loop over the dataset multiple times
@@ -104,7 +123,7 @@ for epoch in range(50):  # loop over the dataset multiple times
         loss.backward()
         optimizer.step()
 
-        running_loss += loss.item() 
+        running_loss += loss.item()
         print('[%d] loss: %.3f' % (i + 1, loss.item()))
 
     print(f"==== EPOCH LOSS: {running_loss/(i+1)}")
--- utilities.py
+++ utilities.py
@@ -1,33 +1,40 @@
 import numpy as np
 
-# square error is usually a poor choice for a classification task such as MNIST 
+# square error is usually a poor choice for a classification task such as MNIST
 # this is the cross entropy loss
 
+
 def cross_entropy_loss(truth, predictions):
-    #return -1/len(batch_truth) * np.inner((batch_truth, np.log(batch_predictions)))  
+    # return -1/len(batch_truth) * np.inner((batch_truth,
+    # np.log(batch_predictions)))
     return -sum(truth*np.log(predictions))
+
 
 def mse(pred, label):
     return np.square(pred-label).mean()
 
+
 def mse_derivative(pred, correct):
     return 2 * (pred - correct)
+
 
 def softmax(inputs):
     return np.exp(inputs)/np.sum(np.exp(inputs))
 
+
 def relu_derivative(arr):
     return (arr > 0) * 1
+
 
 class fully_connected_layer():
     def __init__(self, previous_size, layer_size):
         self.previous_size = previous_size
         self.layer_size = layer_size
         self.weights = np.random.rand(self.previous_size, self.layer_size)
-        #self.bias = np.expand_dims(np.random.rand(self.layer_size), axis=1)
-        self.bias = np.ones([layer_size,1])*0.1
+        # self.bias = np.expand_dims(np.random.rand(self.layer_size), axis=1)
+        self.bias = np.ones([layer_size, 1])*0.1
         self.output_layer = np.random.rand(self.layer_size)
-    
+
     def calculate_output(self, previous_output):
         mult = np.matmul(self.weights.transpose(), previous_output)
         summation = mult + self.bias
@@ -41,22 +48,32 @@ class fully_connected_layer():
         batch_error = cross_entropy_loss(batch_data.labels, batch_output)
         return batch_error
 
+
 class neural_network():
-    def __init__(self, input_size, output_size, number_hidden_layers, hidden_layer_size):
+    def __init__(
+            self, input_size, output_size,
+            number_hidden_layers, hidden_layer_size):
+
         self.input_size = input_size
         self.output_size = output_size
         self.input_layer = np.array(input_size)
         self.number_hidden_layers = np.array(number_hidden_layers)
         self.hidden_layer_size = np.array(hidden_layer_size)
 
-        self.hidden = [] 
+        self.hidden = []
         for i in range(self.number_hidden_layers):
             if i == 0:
-                self.hidden.append(fully_connected_layer(self.input_size, self.hidden_layer_size))
+                self.hidden.append(fully_connected_layer(
+                    self.input_size,
+                    self.hidden_layer_size))
             if i == self.number_hidden_layers-1:
-                self.hidden.append(fully_connected_layer(self.hidden_layer_size, self.output_size))
+                self.hidden.append(fully_connected_layer(
+                    self.hidden_layer_size,
+                    self.output_size))
             else:
-                self.hidden.append(fully_connected_layer(self.hidden_layer_size, self.hidden_layer_size))
+                self.hidden.append(fully_connected_layer(
+                    self.hidden_layer_size,
+                    self.hidden_layer_size))
 
     def predict(self, input_values):
         prediction = np.expand_dims(input_values.flatten(), axis=1)
@@ -66,5 +83,3 @@ class neural_network():
             activations.append(np.array([prediction, activation]))
         prediction = softmax(prediction)
         return prediction, activations
-    
-
